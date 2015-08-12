df1ea102f8e51f827fd6ed44671446e221f6738b
tom <tom@gambitresearch.com>
2015-08-12 13:16:40 +0100

    irssi-dump.py -- add beauty
    Rule of sevens

--- irssi-dump.py
+++ irssi-dump.py
@@ -12,6 +12,19 @@ PARSER = argparse.ArgumentParser(description='Print out new irssi messages')
 PARSER.add_argument('since_seconds', type=int,
                    help='How many seconds worth of messages to get')
 PARSER.add_argument('--exclude-file', type=str, help='File containing regular expressions (python) of messages to not display')
+def main():
+    args = PARSER.parse_args()
+    lines = []
+
+    if args.exclude_file:
+        with open(args.exclude_file) as f:
+            excludes = load_excludes(f)
+    else:
+        excludes = []
+
+    lines = sorted(get_new_lines(time.time() - args.since_seconds, excludes))
+    for line in lines:
+        print(line.strip())
 
 def load_excludes(stream):
     result = []
@@ -23,37 +36,34 @@ def load_excludes(stream):
         result.append(re.compile(line.strip()))
     return result
 
-args = PARSER.parse_args()
+def get_log_files(base_path):
+    for direc, _, files in os.walk(base_path):
+        for filename in files:
+            path = os.path.join(direc, filename)
+            yield filename, path
 
-lines = []
+def get_log_time(line):
+    try:
+        timestamp, _ = line.split('k', 1)
+        timestruct = time.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        return None
+    else:
+        return time.mktime(timestruct)
 
-if args.exclude_file:
-    with open(args.exclude_file) as f:
-        excludes = load_excludes(f)
-else:
-    excludes = []
-
-
-message_start_time = time.time() - args.since_seconds
-for direc, _, files in os.walk(os.path.join(os.environ['HOME'], 'irclogs')):
-    for filename in files:
-        path = os.path.join(direc, filename)
+def get_new_lines(message_start_time, excludes):
+    for channel_name, path in get_log_files(os.path.join(os.environ['HOME'], 'irclogs')):
         with open(path) as stream:
             for line in stream:
-                try:
-                    timestamp, rest = line.split('k', 1)
-                    timestruct = time.strptime(timestamp, '%Y-%m-%dT%H:%M:%S')
-                    when = time.mktime(timestruct)
-
-                    if any(e.search(line) for e in excludes):
-                        continue
-                    elif when < message_start_time:
-                        continue
-                    else:
-                        lines.append(filename + " " + line)
-                except ValueError:
+                when = get_log_time(line)
+                if when is None:
                     continue
+                if any(e.search(line) for e in excludes):
+                    continue
+                elif when < message_start_time:
+                    continue
+                else:
+                    yield channel_name + " " + line
 
-lines.sort()
-for line in lines:
-    print(line.strip())
+if __name__ == '__main__':
+	main()
